26494ba125c23014d22788a79835ccf0d1e9e9f7
BeoGonzalez <beo.gonzalez@duocuc.cl>
2025-09-10 22:53:59 -0300

    Se está probando el push en la rama develop para que el pull request aparezca en el main

--- script.py
+++ script.py
@@ -1,3 +1,3 @@
 #Script de ejemplo para mi prueba de DevOps
 for i in range(5):
-    print(f"El script de Benjamín González ha imprimido este mensaje {i + 1} veces")
+    print(f"El script de Benjamín González ha imprimido este mensaje {i + 1} veces");
